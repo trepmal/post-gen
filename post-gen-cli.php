de3017bea300950dce06d79d7db473d65d79b638
Kailey Lampert <trepmal@gmail.com>
2014-01-25 00:26:31 -0800

    make all  options acceptable, add a few other options

--- post-gen-cli.php
+++ post-gen-cli.php
@@ -8,11 +8,58 @@ class Post_Gen_CLI extends WP_CLI_Command {
 	/**
 	 * Generate posts (More Options than core)
 	 *
+	 * If Image-Gen (https://github.com/trepmal/image-gen) is installed,
+	 * all `wp image-gen create` options are accepted when prefixed with 'img-'
+	 *
 	 * ## OPTIONS
 	 *
 	 * <count>
 	 * : Number of posts
 	 *
+	 * [--paragraphs=<paragraphs>]
+	 * : Single number or range, default 4,7
+	 *
+	 * [--noimage]
+	 * : No image
+	 *
+	 * [--days-offset=<days-offset>]
+	 * : Number of days in past to offset new posts or range, default 1,300,5
+	 *
+	 * [--hours-offset=<hours-offset>]
+	 * : Number of hours in past to offset new posts or range, default 1,24
+	 *
+	 * [--img-height=<img-height>]
+	 * : Height for the image in pixels, default 150
+	 *
+	 * [--img-lowgrey=<img-lowgrey>]
+	 * : Lower grey value (0-255), default 120
+	 *
+	 * [--img-highgrey=<img-highgrey>]
+	 * : Higher grey value (0-255), default 150
+	 *
+	 * [--img-alpha=<img-alpha>]
+	 * : Alpha transparancy (0-127), default 0
+	 *
+	 * [--img-blurintensity=<img-blurintensity>]
+	 * : How often to apply the blur effect, default 2
+	 *
+	 * [--img-filename=<img-filename>]
+	 * : old value
+	 *
+	 * [--img-text=<img-text>]
+	 * : Text to place on the image, default empty
+	 *
+	 * [--img-linespacing=<img-linespacing>]
+	 * : Linespacing in pixels, default 10
+	 *
+	 * [--img-textsize=<img-textsize>]
+	 * : Text size in pixels, default 40
+	 *
+	 * [--img-font=<img-font>]
+	 * : Path to font true type file, default {plugin-path}/fonts/SourceSansPro-BoldIt.otf
+	 *
+	 * [--img-fontcolor=<img-fontcolor>]
+	 * : Font color. Either RGB as an array or a hexcode string, default array(0, 80, 80),
  	 *
 	 * ## EXAMPLES
 	 *
@@ -25,7 +72,7 @@ class Post_Gen_CLI extends WP_CLI_Command {
 		$notify = \WP_CLI\Utils\make_progress_bar( "Generating $count post(s)", $count );
 		$counter = 1;
 		do {
-			post_gen_create_post();
+			post_gen_create_post( $assoc_args );
 			$notify->tick();
 			++$counter;
 		} while ( $counter <= $count);
--- post-gen.php
+++ post-gen.php
@@ -19,33 +19,62 @@
  *
  * @return void
  */
-function post_gen_create_post() {
+function post_gen_create_post( $args = array() ) {
+
+	$defaults = array(
+		'paragraphs'   => '4,7',
+		'noimage'      => 0,
+		'days-offset'  => array( 1, 300, 5 ),
+		'hours-offset' => array( 1, 24 ),
+		'img-height'   => 1000,
+		'img-width'    => 800,
+		'img-lowgrey'  => 150,
+		'img-highgrey' => '',
+	);
+
+	$args = wp_parse_args( $args, $defaults );
+
+	$img_args = array();
+	foreach( $args as $k => $v ) {
+		if ( strpos( $k, 'img-' ) === 0 ) {
+			$new_key = str_replace( 'img-', '', $k );
+			$img_args[ $new_key ] = $v;
+			unset( $args[ $k ] );
+		}
+	}
+
 	$title = post_gen_get_random_title();
-	$day = rand( 1, 100 );
-	$hour = rand( 1, 10 );
+	$day = post_gen_convert_to_value( $args['days-offset'] );
+	$hour = post_gen_convert_to_value( $args['hours-offset'] );
 	$date = date( 'Y-m-d H:i:s', strtotime( "-$day days -$hour hours" ) );
-	$args = array(
+
+	$post_args = array(
 		'post_type'     => 'post',
 		'post_status'   => 'publish',
 		'post_title'    => $title,
-		'post_content'  => post_gen_get_random_content(),
+		'post_content'  => post_gen_get_random_content( $args['paragraphs'] ),
 		'post_author'   => 1,
 		'post_date'     => $date,
 		'post_date_gmt' => $date,
 	);
-	$args = apply_filters( 'post_gen_args', $args );
-	$postid = wp_insert_post( $args );
+	$post_args = apply_filters( 'post_gen_args', $post_args );
+	$postid = wp_insert_post( $post_args );
 	if ( ! is_wp_error( $postid ) ) {
-		if ( function_exists('image_gen__create_image' ) ) {
+		if ( function_exists('image_gen__create_image' ) && ! $args['noimage'] ) {
 			// generate matching image
 
 			// split long title into reasonable lines
 			$image_title = _split_str_by_whitespace( $title, 30 );
 			$image_title = array_map( 'trim', $image_title );
-			// for generation, set lowgrey to 150 (same as highgrey default) to make solid images (because faster)
-			/// see https://github.com/trepmal/image-gen/blob/master/image-gen-cli.php for more args
-			$assoc_args = array( 'width' => 1000, 'height' => 800, 'text' => $image_title, 'lowgrey' => 150 );
-			$imageid = image_gen__create_image( $title, $assoc_args );
+
+			$img_args = array_map( 'post_gen_convert_to_value', $img_args );
+
+			// if no highgrey, make the same as low. this will make the image solid and faster to generate
+			$img_args['highgrey'] = empty( $img_args['highgrey'] ) ? $img_args['lowgrey'] : $img_args['highgrey'];
+
+			$img_args['text'] = $image_title;
+
+			$imageid = image_gen__create_image( $title, $img_args );
 			// make featured
 			set_post_thumbnail ( $postid, $imageid );
 
@@ -70,14 +99,13 @@ function post_gen_create_posts( $count = 1 ) {
 	do {
 		post_gen_create_post();
 		++$counter;
-	} while ( $counter <= $count);
-
+	} while ( $counter <= $count );
 }
 
 /**
  * Get random title from title list
  *
- * @return void
+ * @return string HTML
  */
 function post_gen_get_random_title() {
 	$titles = file_get_contents( plugin_dir_path(__FILE__) .'/lorem-titles.txt' );
@@ -90,18 +118,55 @@ function post_gen_get_random_title() {
 /**
  * Get random content from paragraph list
  *
- * @return void
+ * @param int|array $number_paragraphs One number, or array of 2
+ * @return string HTML
  */
-function post_gen_get_random_content() {
+function post_gen_get_random_content( $number_paragraphs = array( 3, 6 ) ) {
 	$paras = file_get_contents( plugin_dir_path(__FILE__) .'/lorem-paragraphs.txt' );
 	$paras = explode( "\n", $paras );
 	shuffle( $paras );
 
-	$paras = array_slice( $paras, 0, rand( 3, 6 ) );
+	$num = post_gen_convert_to_value( $number_paragraphs );
+
+	$paras = array_slice( $paras, 0, $num );
 
 	return implode( "\n\n", $paras );
 }
 
+/**
+ * Helper function.
+ *
+ * Convert given input to single value
+ * Input could be single value, comma-separated string, array
+ *
+ * @param int|string|array $input
+ * @return int|string
+ */
+function post_gen_convert_to_value( $input ) {
+	// if not array, explode
+	if ( ! is_array( $input ) ) {
+		$input = explode( ',', $input );
+	}
+
+	// if a third param, assume last is incremental value
+	// get range, randomize, return
+	if ( isset( $input[2] ) ) {
+		$incr = array_pop( $input );
+		sort( $input );
+		$range = range( $input[0], $input[1], $incr );
+		shuffle( $range );
+		return array_shift( $range );
+	}
+	// otherwise, just get random in range
+	if ( isset( $input[1] ) ) {
+		sort( $input );
+		$input = rand( $input[0], $input[1] );
+	} else {
+		$input = $input[0];
+	}
+
+	return $input;
+}
 
 if ( defined('WP_CLI') && WP_CLI ) {
 	include plugin_dir_path( __FILE__ ) . '/post-gen-cli.php';
